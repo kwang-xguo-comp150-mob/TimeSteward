a44b0a1c4025eb591211f67e6bda8b19c38b71f7
Xiaozheng(Will) Guo <xguo.tufts@gmail.com>
2017-10-05 19:59:53 -0400

    light modify the naming of some variables.

--- android_studio/app/src/main/java/edu/tufts/cs/kwangxguo/timesteward/SettingActivity.java
+++ android_studio/app/src/main/java/edu/tufts/cs/kwangxguo/timesteward/SettingActivity.java
@@ -1,21 +1,11 @@
 package edu.tufts.cs.kwangxguo.timesteward;
 
 import android.app.Activity;
-import android.content.Context;
-import android.content.Intent;
 import android.content.pm.ApplicationInfo;
-import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
-import android.graphics.Color;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.view.View;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.NumberPicker;
@@ -31,39 +21,39 @@ public class SettingActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
+
+        /*******************************************************
+         *             Create App List:
+         *   Using customized adapter, display icon and app name
+         *******************************************************/
         packageManager = getPackageManager();
         List<ApplicationInfo> apps = packageManager.getInstalledApplications(0);
         List<ApplicationInfo> installedApps = new ArrayList<ApplicationInfo>();
         List<String> appsNames = new ArrayList<String>();
         for(ApplicationInfo app : apps) {
-            //checks for flags; if flagged, check if updated system app
-            if((app.flags & ApplicationInfo.FLAG_UPDATED_SYSTEM_APP) != 0) {
+            // check if the app is a system app
+            if((app.flags & ApplicationInfo.FLAG_SYSTEM) == 0 && (app.flags & ApplicationInfo.FLAG_UPDATED_SYSTEM_APP) == 0) {
                 installedApps.add(app);
                 appsNames.add((String)packageManager.getApplicationLabel(app));
-                //System.out.println((String)packageManager.getApplicationLabel(app));
-                //it's a system app, not interested
-            } else if ((app.flags & ApplicationInfo.FLAG_SYSTEM) != 0) {
-                //Discard this one
-                //in this case, it should be a user-installed app
-            } else {
-                installedApps.add(app);
-                appsNames.add((String)packageManager.getApplicationLabel(app));
-                //System.out.println((String)packageManager.getApplicationLabel(app));
+                Log.d("Setting, app-list", (String)packageManager.getApplicationLabel(app));
             }
         }
 
-        ArrayAdapter adapter = new ArrayAdapter<String>(this,
-                R.layout.activity_applistview, appsNames);
+        // the 2nd parameter is a template view xml
+        ArrayAdapter adapter = new ArrayAdapter<String>(this, R.layout.setting_applist_item, appsNames);
         ListView listView = (ListView)findViewById(R.id.applist);
         /* set the height of the listView */
         LinearLayout.LayoutParams lp = (LinearLayout.LayoutParams)listView.getLayoutParams();
         System.out.print(appsNames.size());
         lp.height = 100 * appsNames.size();
         listView.setLayoutParams(lp);
-
         listView.setAdapter(adapter);
-        final TextView tv = (TextView) findViewById(R.id.tv);
-        NumberPicker np = (NumberPicker) findViewById(R.id.np);
+
+        /***********************************
+        *          Set Time Limit          *
+        ************************************/
+        final TextView tv = findViewById(R.id.set_time_limit);
+        NumberPicker np = findViewById(R.id.np);
         //Set TextView text color
         //tv.setTextColor(Color.parseColor("#ffd32b3b"));
 
