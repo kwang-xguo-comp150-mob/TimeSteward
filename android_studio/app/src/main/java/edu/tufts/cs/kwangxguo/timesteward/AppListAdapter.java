a60d1e8b654f4f22c597a60778984d10463ad53c
Xiaozheng(Will) Guo <xguo.tufts@gmail.com>
2017-10-06 00:59:32 -0400

    prepare to write customized onCheckedChangeListener

--- android_studio/app/src/main/java/edu/tufts/cs/kwangxguo/timesteward/AppListAdapter.java
+++ android_studio/app/src/main/java/edu/tufts/cs/kwangxguo/timesteward/AppListAdapter.java
@@ -4,10 +4,12 @@ import android.content.Context;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
 import android.graphics.drawable.Drawable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -33,17 +35,23 @@ class AppListAdapter extends ArrayAdapter {
             convertView = LayoutInflater.from(getContext()).inflate(R.layout.setting_applist_item, parent, false);
         }
         // look up views
-        ImageView appIcon = (ImageView)convertView.findViewById(R.id.app_icon);
-        TextView appName = (TextView)convertView.findViewById(R.id.app_name);
+        ImageView appIcon = convertView.findViewById(R.id.app_icon);
+        TextView appName = convertView.findViewById(R.id.app_name);
         // populate data into template view
         String appNameString = (String)pm.getApplicationLabel(app);
-        Drawable appIconDrawable = (Drawable)app.loadIcon(pm);
+        Drawable appIconDrawable = app.loadIcon(pm);
         appIcon.setImageDrawable(appIconDrawable);
         appName.setText(appNameString);
 
         // deal with check box
-
-
+        CheckBox cBox = convertView.findViewById(R.id.app_checkbox);
+        cBox.setTag(app);
+        cBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton cBox, boolean isChecked) {
+                if (isChecked) Log.d("cBox", "onCheckedChanged: checked: " + cBox.getTag());
+            }
+        });
         return convertView;
     }
 
