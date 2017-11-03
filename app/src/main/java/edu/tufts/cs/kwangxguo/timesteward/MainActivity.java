15665ca6f522d95969b69f7c036832c8afd3af19
KeyueWang92 <keyuewang1014@gmail.com>
2017-11-03 16:28:54 -0400

    use(add) Crashlytics

--- app/src/main/java/edu/tufts/cs/kwangxguo/timesteward/MainActivity.java
+++ app/src/main/java/edu/tufts/cs/kwangxguo/timesteward/MainActivity.java
@@ -10,12 +10,15 @@ import android.content.Intent;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
 
 public class MainActivity extends AppCompatActivity {
     Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Fabric.with(this, new Crashlytics());
         setContentView(R.layout.activity_main);
         button = (Button)findViewById(R.id.button2);
         addListenerOnButton();
