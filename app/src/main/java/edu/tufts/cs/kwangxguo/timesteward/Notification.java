32d37b13eccece14ec25701eaa58ab2f288ede20
KeyueWang92 <keyuewang1014@gmail.com>
2017-11-15 17:11:53 -0500

    finish the notification, should store the data in sqlite and update the backgroundmonitor

--- app/src/main/java/edu/tufts/cs/kwangxguo/timesteward/Notification.java
+++ app/src/main/java/edu/tufts/cs/kwangxguo/timesteward/Notification.java
@@ -11,7 +11,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class Notification extends AppCompatActivity {
-    private SeekBar sb;
+    private SeekBar sb1,sb2,sb3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,23 +22,31 @@ public class Notification extends AppCompatActivity {
         Cursor cursor = db.rawQuery("SELECT * FROM Setting", null);
         cursor.moveToFirst();
         final int timeLimit = cursor.getInt(1);
-        sb = findViewById(R.id.seekBar);
-        sb.setMax(timeLimit);
+        sb1 = findViewById(R.id.seekBar);
+        sb2 = findViewById(R.id.seekBar2);
+        sb3 = findViewById(R.id.seekBar3);
+        sb1.setMax(timeLimit);
 
         //it should get from the sqlite
-        int default_startPoint = 1;
-
-        sb.setProgress(default_startPoint);
+        int default_startPoint = 0;
+        sb2.setMax(default_startPoint);
+        sb3.setMax(timeLimit-default_startPoint);
+        sb1.setProgress(default_startPoint);
         TextView t = findViewById(R.id.textView13);
         t.setText(default_startPoint+" mins");
         t = findViewById(R.id.textView12);
         t.setText(timeLimit+"mins (your time limit)");
-        sb.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener(){
+        t = findViewById(R.id.textView16);
+        t.setText("Start Point: " + default_startPoint+" mins");
+        t = findViewById(R.id.textView19);
+        t.setText("Left Time: " + (timeLimit - default_startPoint)+" mins");
+        //the interval should be stored in sqlite
+        sb1.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener(){
             int progressChangedValue = 0;
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
                 progressChangedValue = progress;
                 TextView t = findViewById(R.id.textView13);
-                t.setText("Every " + progressChangedValue+" mins");
+                t.setText("In " + progressChangedValue+" mins");
             }
 
             public void onStartTrackingTouch(SeekBar seekBar) {
@@ -46,8 +54,54 @@ public class Notification extends AppCompatActivity {
             }
 
             public void onStopTrackingTouch(SeekBar seekBar) {
-                Toast.makeText(Notification.this, "You just chose the alarm interval:"
+                Toast.makeText(Notification.this, "You just chose the intense alarm start point:"
                                 + progressChangedValue + "mins", Toast.LENGTH_SHORT).show();
+                sb2.setMax(progressChangedValue);
+                sb3.setMax(timeLimit - progressChangedValue);
+                TextView t = findViewById(R.id.textView16);
+                t.setText("Start Point: " + progressChangedValue+" mins");
+                t = findViewById(R.id.textView19);
+                t.setText("Left Time: " + (timeLimit - progressChangedValue)+" mins");
+                //the interval should be stored in sqlite
+            }
+        });
+
+        sb2.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            int progressChangedValue = 0;
+
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                progressChangedValue = progress;
+                TextView t = findViewById(R.id.textView15);
+                t.setText("Every " + progressChangedValue + " mins");
+            }
+
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                // TODO Auto-generated method stub
+            }
+
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                Toast.makeText(Notification.this, "You just chose the gentle alarm interval:"
+                        + progressChangedValue + "mins", Toast.LENGTH_SHORT).show();
+                //the interval should be stored in sqlite
+            }
+        });
+
+        sb3.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            int progressChangedValue = 0;
+
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                progressChangedValue = progress;
+                TextView t = findViewById(R.id.textView18);
+                t.setText("Every " + progressChangedValue + " mins");
+            }
+
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                // TODO Auto-generated method stub
+            }
+
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                Toast.makeText(Notification.this, "You just chose the intense alarm interval:"
+                        + progressChangedValue + "mins", Toast.LENGTH_SHORT).show();
                 //the interval should be stored in sqlite
             }
         });
